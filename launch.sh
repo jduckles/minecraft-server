be22faf47161aae2da884aaa72d48dd458c23705
Jonah Duckles <jonah@duckles.org>
2015-02-27 17:00:44 -0600

    Some fixes

--- launch.sh
+++ launch.sh
@@ -1,34 +1,48 @@
 #!/bin/bash
 S3_BUCKET=s3://jduck-minecraft-backup
 WORLD=duckland-creative
+DOCKER=jduckles/minecraft
+VM_NAME="minecraft"
 
 # Uses the tugboat app for DigitalOcean to bring up the VM.
-if [ -n $(tugboat droplets |grep minecraft) ]; then
-  tugboat create minecraft -s 64 -i 10581649 -r 1 -k 14937
+if [ -z "$(tugboat droplets |grep ${VM_NAME})" ] ; then
+  tugboat create ${VM_NAME} -s 64 -i 10581649 -r 1 -k 14937
   tugboat info minecraft |grep IP: | sed 's/IP:               //g'
-else;
+  # Wait for VM to exit 'new' status before proceeding
+  echo "Bringing up the server, this could take a few:"
+  sleep 5
+  while [ -n "$(tugboat info ${VM_NAME} | grep "Status" | grep "new" )" ] ; do
+    echo -n .
+    sleep 5
+  done
+else
   echo "looks like it is already running"
 fi
 
-# Wait for VM to exit new status
-echo "Bringing up the server, this could take a few:"
-while [[ -n $(tugboat info minecraft | grep "Status" | grep "new" ) ]]; do
-  echo -n .
-  sleep 5
-done
 
-# Grab the VM's IP
-VM_IP=$(tugboat info minecraft |grep IP: | sed 's/IP:               //g')
 
+
+
+# Grab the VMs IP
+VM_IP=$(tugboat info ${VM_NAME} |grep IP: | sed 's/IP:               //g')
+
+sed -i .old "/${VM_IP}/d" ~/.ssh/known_hosts
+
+SSH_COMMAND="ssh root@${VM_IP}"
 ## Pull s3backup
-scp ~/.s3cfg root@${VM_IP}:.
-ssh ${VM_IP} apt-get install -y s3cmd
+${SSH_COMMAND} ~/.s3cfg root@${VM_IP}:.
+${SSH_COMMAND} apt-get install -y s3cmd ufw
 
 # Sync server data
-ssh root@${VM_IP} mkdir -m 777 -p /opt/msm/servers
-ssh root@${VM_IP} s3cmd sync ${S3_BUCKET} /opt/msm/servers
+${SSH_COMMAND} mkdir -m 777 -p /opt/msm/servers
+# Sync worlds from last time the server was up
+${SSH_COMMAND} s3cmd sync ${S3_BUCKET} /opt/msm/servers
+# bring up firewall and poke a hole
+${SSH_COMMAND} ufw enable && ufw allow 25565/tcp
 
-VOLUMES="-v /opt/msm/server:/opt/msm/servers"
-DOCKER_OPTS="--net="host" -d jduckles/minecraft"
+# Mount the games volumes with server backups
+VOLUMES="-v /opt/msm/servers:/opt/msm/servers"
+DOCKER_OPTS="--net="host" -d ${DOCKER}"
 COMMAND="exec msm ${WORLD} start"
 DOCKER_RUN="docker run ${VOLUMES} ${DOCKER_OPTS} ${COMMAND}"
+${SSH_COMMAND} ${DOCKER_RUN}
